9de1afddcb80fb8b8ff169a25f3d1a2616c95e44
EscaperX <ycuangwhen@gmail.com>
2023-08-17 18:42:51 +0800

    feat: Add submodule and test modular cmake

--- cmake/3rdparty.cmake
+++ cmake/3rdparty.cmake
@@ -1,3 +1,185 @@
-function(add_header_only_package)
-    
-endfunction(add_header_only_package)
+set(3RD_DIR ${CMAKE_SOURCE_DIR}/3rdparty CACHE PATH "" FORCE)
+set(3RD_SOURCE_DIR ${3RD_DIR}/ CACHE PATH "" FORCE)
+set(3RD_BINARY_DIR ${CMAKE_BINARY_DIR}/ThirdPartyBuild CACHE PATH "" FORCE)
+set(3RD_INSTALL_DIR ${CMAKE_BINARY_DIR}/ThirdPartyInstall CACHE PATH "" FORCE)
+
+function(expand_3rd_path_expression)
+    cmake_parse_arguments(PARAMS "" "OUTPUT;SOURCE_DIR;BINARY_DIR;INSTALL_DIR" "INPUT" ${ARGN})
+
+    foreach(I ${PARAMS_INPUT})
+        set(TEMP "${I}")
+        if (DEFINED PARAMS_SOURCE_DIR)
+            string(REPLACE "$<SOURCE_DIR>" ${PARAMS_SOURCE_DIR} TEMP ${TEMP})
+            message("${PARAMS_SOURCE_DIR}")
+        endif()
+        if (DEFINED PARAMS_BINARY_DIR)
+            string(REPLACE "$<BINARY_DIR>" ${PARAMS_BINARY_DIR} TEMP ${TEMP})
+            message("${PARAMS_BINARY_DIR}")
+        endif()
+        if (DEFINED PARAMS_INSTALL_DIR)
+            string(REPLACE "$<INSTALL_DIR>" ${PARAMS_INSTALL_DIR} TEMP ${TEMP})
+            message("${PARAMS_INSTALL_DIR}")
+        endif()
+        list(APPEND RESULT ${TEMP})
+    endforeach()
+    message("${RESULT}")
+    message("")
+    set(${PARAMS_OUTPUT} ${RESULT} PARENT_SCOPE)
+endfunction()
+
+function(get_3rd_platform_path)
+    cmake_parse_arguments(PARAMS "" "OUTPUT" "INPUT" ${ARGN})
+
+    set(PLATFORM_KEYWORDS "Windows;Darwin;Linux")
+
+    # match a platform
+    set(HAS_KEYWORDS FALSE)
+    foreach (I ${PARAMS_INPUT})
+        foreach (K ${PLATFORM_KEYWORDS})
+            if (${I} STREQUAL ${K})
+                set(HAS_KEYWORDS TRUE)
+                set(RESULT ${I})
+                break()
+            endif()
+        endforeach()
+
+        if (${HAS_KEYWORDS})
+            break()
+        endif()
+    endforeach()
+
+    # I don't understand
+#     set(START_LOG FALSE)
+#     foreach (I ${PARAMS_INPUT})
+#         if ((NOT ${START_LOG}) AND (${I} STREQUAL ${CMAKE_SYSTEM_NAME}))
+#             set(START_LOG TRUE)
+#             continue()
+#         endif()
+#
+#         if (NOT ${START_LOG})
+#             continue()
+#         endif()
+#
+#         set(END_LOG FALSE)
+#         foreach (K ${PLATFORM_KEYWORDS})
+#             if (${K} STREQUAL ${I})
+#                 set(END_LOG TRUE)
+#                 break()
+#             endif()
+#         endforeach()
+#
+#         if (${END_LOG})
+#             break()
+#         endif()
+#
+#         list(APPEND RESULT ${I})
+#     endforeach ()
+
+    if (${HAS_KEYWORDS})
+        set(${PARAMS_OUTPUT} ${RESULT} PARENT_SCOPE)
+    else()
+        set(${PARAMS_OUTPUT} ${PARAMS_INPUT} PARENT_SCOPE)
+    endif()
+endfunction()
+
+function(add_cmake_project)
+    cmake_parse_arguments(PARAMS "" "NAME;PLATFORM;" "CMAKE_ARG;INCLUDE;LINK;LIB;RUNTIME_DEP" ${ARGN})
+
+    if ((NOT (${PARAMS_PLATFORM} STREQUAL "All")) AND (NOT (${PARAMS_PLATFORM} STREQUAL ${CMAKE_SYSTEM_NAME})))
+        return()
+    endif()
+
+    set(NAME "${PARAMS_NAME}")
+    set(FULL_NAME "${PARAMS_NAME}-${PARAMS_PLATFORM}")
+    set(SOURCE_DIR "${3RD_SOURCE_DIR}/${NAME}")
+    set(BINARY_DIR "${3RD_BINARY_DIR}/${NAME}")
+    set(INSTALL_DIR "${3RD_INSTALL_DIR}/${NAME}/$<CONFIG>")
+
+    ExternalProject_Add(
+        ${NAME}
+        SOURCE_DIR ${SOURCE_DIR}
+        BINARY_DIR ${BINARY_DIR}
+        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} ${PARAMS_CMAKE_ARG}
+        BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --config $<CONFIG>
+        INSTALL_COMMAND ${CMAKE_COMMAND} --install <BINARY_DIR> --config $<CONFIG>
+    )
+    set_target_properties(
+        ${NAME} PROPERTIES
+        3RD_TYPE "CMakeProject"
+    )
+
+    if (DEFINED PARAMS_INCLUDE)
+        expand_3rd_path_expression(
+            INPUT ${PARAMS_INCLUDE}
+            OUTPUT R_INCLUDE
+            SOURCE_DIR ${SOURCE_DIR}
+            BINARY_DIR ${BINARY_DIR}
+            INSTALL_DIR ${INSTALL_DIR}
+        )
+        get_3rd_platform_path(
+            INPUT ${R_INCLUDE}
+            OUTPUT P_INCLUDE
+        )
+        set_target_properties(
+            ${NAME} PROPERTIES
+            3RD_INCLUDE "${P_INCLUDE}"
+        )
+        message("INCLUDE path: ${P_INCLUDE}")
+    endif()
+
+    if (DEFINED PARAMS_LINK)
+        expand_3rd_path_expression(
+            INPUT ${PARAMS_LINK}
+            OUTPUT R_LINK
+            SOURCE_DIR ${SOURCE_DIR}
+            BINARY_DIR ${BINARY_DIR}
+            INSTALL_DIR ${INSTALL_DIR}
+        )
+        get_3rd_platform_path(
+            INPUT ${R_LINK}
+            OUTPUT P_LINK
+        )
+        set_target_properties(
+            ${NAME} PROPERTIES
+            3RD_LINK "${P_LINK}"
+        )
+        message("LINK path: ${P_LINK}")
+    endif()
+
+    if (DEFINED PARAMS_LIB)
+        expand_3rd_path_expression(
+            INPUT ${PARAMS_LIB}
+            OUTPUT R_LIB
+            SOURCE_DIR ${SOURCE_DIR}
+            BINARY_DIR ${BINARY_DIR}
+            INSTALL_DIR ${INSTALL_DIR}
+        )
+        get_3rd_platform_path(
+            INPUT ${R_LIB}
+            OUTPUT P_LIB
+        )
+        set_target_properties(
+            ${NAME} PROPERTIES
+            3RD_LIB "${P_LIB}"
+        )
+        message("LIB path: ${P_LIB}")
+    endif()
+
+    if (DEFINED PARAMS_RUNTIME_DEP)
+        expand_3rd_path_expression(
+            INPUT ${PARAMS_RUNTIME_DEP}
+            OUTPUT R_RUNTIME_DEP
+            SOURCE_DIR ${SOURCE_DIR}
+            BINARY_DIR ${BINARY_DIR}
+            INSTALL_DIR ${INSTALL_DIR}
+        )
+        get_3rd_platform_path(
+            INPUT ${R_RUNTIME_DEP}
+            OUTPUT P_RUNTIME_DEP
+        )
+        set_target_properties(
+            ${NAME} PROPERTIES
+            3RD_RUNTIME_DEP "${P_RUNTIME_DEP}"
+        )
+    endif()
+endfunction(add_cmake_project)
---
+++ cmake/target.cmake
@@ -0,0 +1,95 @@
+function(my_link_libraries)
+    cmake_parse_arguments(PARAMS "" "NAME" "LIB" ${ARGN})
+
+    foreach(L ${PARAMS_LIB})
+        if (NOT (TARGET ${L}))
+            target_link_libraries(${PARAMS_NAME} ${L})
+            continue()
+        endif()
+
+        get_target_property(3RD_TYPE ${L} 3RD_TYPE)
+        if (${3RD_TYPE} STREQUAL "3RD_TYPE-NOTFOUND")
+            target_link_libraries(${PARAMS_NAME} ${L})
+        else()
+            get_target_property(INCLUDE ${L} 3RD_INCLUDE)
+            get_target_property(LINK ${L} 3RD_LINK)
+            get_target_property(LIB ${L} 3RD_LIB)
+
+            if (NOT ("${INCLUDE}" STREQUAL "INCLUDE-NOTFOUND"))
+                target_include_directories(${PARAMS_NAME} PUBLIC ${INCLUDE})
+            endif()
+            if (NOT ("${LINK}" STREQUAL "LINK-NOTFOUND"))
+                target_link_directories(${PARAMS_NAME} PUBLIC ${LINK})
+            endif()
+            if (NOT ("${LIB}" STREQUAL "LIB-NOTFOUND"))
+                target_link_libraries(${PARAMS_NAME} ${LIB})
+            endif()
+
+            if (${3RD_TYPE} STREQUAL "CMakeProject")
+                add_dependencies(${PARAMS_NAME} ${L})
+            endif()
+        endif()
+
+        get_target_property(RUNTIME_DEP ${L} 3RD_RUNTIME_DEP)
+        if (NOT ("${RUNTIME_DEP}" STREQUAL "RUNTIME_DEP-NOTFOUND"))
+            CombineRuntimeDependencies(
+                NAME ${PARAMS_NAME}
+                RUNTIME_DEP "${RUNTIME_DEP}"
+            )
+        endif()
+    endforeach()
+endfunction(my_link_libraries)
+
+function(my_add_executable)
+    cmake_parse_arguments(PARAMS "SAMPLE;META" "NAME" "SRC;INC;LINK;LIB;DEP_TARGET;RES" ${ARGN})
+
+    if (${PARAMS_SAMPLE} AND (NOT ${BUILD_SAMPLE}))
+        return()
+    endif()
+
+    # if (DEFINED PARAMS_REFLECT)
+    #     AddMirrorInfoSourceGenerationTarget(
+    #         NAME ${PARAMS_NAME}
+    #         OUTPUT_SRC GENERATED_SRC
+    #         OUTPUT_TARGET_NAME GENERATED_TARGET
+    #         SEARCH_DIR ${PARAMS_REFLECT}
+    #         PRIVATE_INC ${PARAMS_INC}
+    #         LIB ${PARAMS_LIB}
+    #     )
+    # endif()
+
+    add_executable(
+        ${PARAMS_NAME}
+        ${PARAMS_SRC}
+        ${GENERATED_SRC}
+    )
+    target_include_directories(
+        ${PARAMS_NAME}
+        PRIVATE ${PARAMS_INC}
+    )
+    target_link_directories(
+        ${PARAMS_NAME}
+        PRIVATE ${PARAMS_LINK}
+    )
+    my_link_libraries(
+        NAME ${PARAMS_NAME}
+        LIB ${PARAMS_LIB}
+    )
+    # AddRuntimeDependenciesCopyCommand(
+    #     NAME ${PARAMS_NAME}
+    # )
+    # AddResourcesCopyCommand(
+    #     NAME ${PARAMS_NAME}
+    #     RES ${PARAMS_RES}
+    # )
+    if (DEFINED PARAMS_DEP_TARGET)
+        add_dependencies(${PARAMS_NAME} ${PARAMS_DEP_TARGET})
+    endif()
+    # if (DEFINED PARAMS_REFLECT)
+    #     add_dependencies(${PARAMS_NAME} ${GENERATED_TARGET})
+    # endif()
+
+    if (${MSVC})
+        set_target_properties(${PARAMS_NAME} PROPERTIES VS_DEBUGGER_WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/$<CONFIG>)
+    endif()
+endfunction(my_add_executable)
